46484d13a28579387cc4501116c5605a54772c61
madhavapriyadharshini <madhavpriya0000@gmail.com>
2025-06-11 14:07:12 +0530

    Add index.php for UI

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Product Search</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h2>Search Products</h2>
+  <input type="text" id="search" placeholder="Type to search...">
+  <div id="result"></div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
